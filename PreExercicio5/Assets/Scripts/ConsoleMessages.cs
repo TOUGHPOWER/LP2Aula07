8268674006f43659bfdebe2172119cca122cdabc
Pedroosorio97 <pedromrnosorio97@gmail.com>
2022-11-22 12:00:39 +0000

    Complete Ex5

---
+++ PreExercicio5/Assets/Scripts/ConsoleMessages.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleMessages : MonoBehaviour
+{
+    private EventMaster em;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        em = GetComponent<EventMaster>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        em.KeyPress += ShowConsoleMessage;
+    }
+
+    private void OnDisable()
+    {
+        em.KeyPress -= ShowConsoleMessage;
+    }
+
+    private void ShowConsoleMessage(char key)
+    {
+        print(key);
+    }
+}
